b0af330a5ad3f66060f1e80bd65727d6f9b22f11
Calcifer <calciferzh@outlook.com>
2017-06-29 20:36:22 +0800

    hotfix: append field to ImageTag

--- ImageManagement/images/models.py
+++ ImageManagement/images/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 class ImageTag(models.Model):
     name = models.CharField(max_length=20)
+    images = models.ManyToManyField('images.ImagePost', blank=True)
 
     def __str__(self):
         return self.name
