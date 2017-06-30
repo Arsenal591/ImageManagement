9e19f73cc625a72794c0d4925e9e554dcd260bd4
Calcifer <calciferzh@outlook.com>
2017-06-30 10:02:00 +0800

    adjust DB structure

--- ImageManagement/images/models.py
+++ ImageManagement/images/models.py
@@ -3,19 +3,24 @@ from django.utils import timezone
 
 class ImageTag(models.Model):
     name = models.CharField(max_length=20)
-    images = models.ManyToManyField('images.ImagePost', blank=True)
+    images = models.ManyToManyField('ImagePost')
 
     def __str__(self):
         return self.name
 
 class ImagePost(models.Model):
+    # basic info
     author = models.ForeignKey('auth.User', null=True)
-    tags = models.ManyToManyField(ImageTag, blank=True)
-    path = models.CharField(max_length=100)
+    tags = models.ManyToManyField(ImageTag)
+    path = models.CharField(max_length=128)
     is_public = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=timezone.now)
     heat = models.IntegerField(default=0)
-    
+    description = models.CharField(max_length=140, null=True)
+    # relationships
+    siblings = models.ManyToManyField('ImagePost')
+    comments = models.ManyToManyField('ImageComment')
+
     def __str__(self):
         return self.path
 
@@ -23,6 +28,8 @@ class ImageComment(models.Model):
     image = models.ForeignKey(ImagePost, on_delete=models.CASCADE)
     content = models.CharField(max_length=140)
     author = models.ForeignKey('auth.User')
+    previous = models.ForeignKey('ImageComment', null=True)
+    pub_time = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return content
