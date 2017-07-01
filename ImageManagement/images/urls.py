e862c154d82cddcaedd51c85312c6ff0e8ac67bb
Calcifer <calciferzh@outlook.com>
2017-07-01 22:31:34 +0800

    finish basic logic of processing

--- ImageManagement/images/urls.py
+++ ImageManagement/images/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^upload$', views.upload, name='upload'),
-    url(r'^upload_img$', views.upload_img, name='upload_img')
+    url(r'^pool$', views.img_pool, name='pool'),
+    url(r'process/([1-9][0-9]*)$', views.process, name='process')
 ]
--- ImageManagement/images/views.py
+++ ImageManagement/images/views.py
@@ -3,46 +3,68 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from .models import ImageTag,  ImagePost
+from .models import ImageTag, ImagePost
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.core.files import File
 import time
-import random
 import os
 from .forms import *
+from .slave import *
 from scipy import misc
 from django.http import HttpResponse
+from django.conf import settings
 
-def index(request):
+def img_pool(request):
     imgs = ImagePost.objects.order_by('-created_at')
     pub_imgs = [img for img in imgs if img.is_public]
-    return render(request, 'index.html', {'imgs': pub_imgs})
+    pub_imgs = pub_imgs[:6]
+    return render(request, 'pool.html', {'imgs': pub_imgs})
 
 def upload(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
-        if form.is_valid():
+        if True: # lazy to delete this line
             post = form.save(commit=False)
+            # need to add user info here
             post.save()
-            return render(request, 'success.html')
+            return redirect('process', post.id)
     return render(request, 'upload.html', {'form': UploadForm()})
 
-def upload_img(request):
-    if request.method == 'POST':
-        img = request.FILES['image']
-        if img:
-            img_file = misc.imread(img)
-            img_suffix = str(img).split('.')[-1]
-            series_num = str(time.time()).replace('.', '') + str(random.randint(0, 1000))
-            save_path = '/static/images/'+series_num+'.'+img_suffix
-            misc.imsave(os.getcwd()+'/images'+save_path, img_file)
-            po = ImagePost()
-            po.path = save_path
-            po.created_at = timezone.now()
-            po.save()
-            return render(request, 'success.html')
-        return render(requese, 'fail.html')
+def create_img(img_post, method, **kwargs):
+    new_post = ImagePost()
 
+    img_dir = os.getcwd() + '/images/' + settings.MEDIA_DIR
+    img_name = str(img_post.img)
+    img_file = misc.imread(img_dir + img_name)
+
+    new_img = gray(img_file)
+    if method == 'gray':
+         new_img = gray(img_file) 
+    new_path = img_dir + img_name.split('.')[0] + '_' + method + '.jpg'
+    misc.imsave(new_path, new_img)
+
+    new_post.img = File(new_path)
+    new_post.author = img_post.author
+    new_post.is_public = img_post.is_public
+    new_post.parent = img_post
+    for tag in img_post.tags.all():
+        new_post.tags.add(tag)
+    new_post.save()
+
+def process(request, img_id):
+    img_post = get_object_or_404(ImagePost, pk=img_id)
+    form = ProcessForm(request.POST)
+    if form.is_valid():
+        if form['gray']:
+            create_img(img_post, 'gray')
+#        if form['blur'] != 0:
+#            create_img(img_post, 'blur', degree=form['blur'])
+        return render(request, 'success.html')
+        # if form.binaryzation
+            
     
-
+    return render(request, 'process.html', {'img': img_post, 'form': ProcessForm()})
+        
 
