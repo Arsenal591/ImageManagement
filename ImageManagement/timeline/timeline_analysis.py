44e826c71f50c9b2a01be2d84ca3041b2bf04c19
Arsenal591 <arsenal591@126.com>
2017-07-02 23:27:06 +0800

    fix database query; fix somebugs; FATAL: have NOT been thoroughly tested

--- ImageManagement/timeline/timeline_analysis.py
+++ ImageManagement/timeline/timeline_analysis.py
@@ -2,33 +2,46 @@ from .timeline_generator import *
 from images.models import ImagePost
 from users.models import MyUser
 
+# 图片image被点了几次赞
 def get_image_like_num(image):
     likes = image.timeline_set.filter(type='like')
     return likes.count()
 
+# 图片image被收藏了几次
 def get_image_collect_num(image):
     collects = image.timeline_set.filter(type='collect')
     return collects.count()
 
+# 图片image被评论了几次
 def get_image_comment_num(image):
     comments = image.timeline_set.filter(type='comment')
     return comments.count()
 
+# 找到给图片点赞的所有用户
+# 返回值：由MyUser类的对象组成的列表
 def get_image_liked(image):
-    timelines = Timeline.objects.filter(image_id=image, type='like').order_by('occur_time')
+    # 找到与该图片有关的点赞时间线，并按时间排序
+    timelines = image.timeline_set.filter(type='like').order_by('occur_time')
+    # 找到这些时间线的发出者，即点赞者
     users = timelines.values_list('sender_id', flat=True)
     return users
 
+# 找到收藏该图片的所有用户
+# 返回值：由MyUser类的对象组成的列表
 def get_image_collected(image):
-    timelines = Timeline.objects.filter(image_id=image, type='collect').order_by('occur_time')
+    timelines = image.timeline_set.filter(type='collect').order_by('occur_time')
     users = timelines.values_list('sender_id', flat=True)
     return users
 
+# 找到评论该照片的所有用户、对应的评论、和发生时间
+# 返回值：由字典组成的列表，每个字典都有三个字段，对应上面的三个元素
 def get_image_commented(image):
-    timelines = Timeline.objects.filter(image_id=image, type='comment').order_by('occur_time')
-    result = timelines.values_list('sender_id', 'comment_text')
+    timelines = image.timeline_set.filter(type='comment').order_by('occur_time')
+    result = timelines.values_list('sender_id', 'comment_text', 'occur_time')
     return result
 
+# 找到user收藏的图片集合
+# 返回值：由图片组成的列表
 def get_image_collection(user):
-    timelines = Timeline.objects.filter(sender_id=user, type='collect').order_by('occur_time')
+    timelines = user.sends.filter(type='collect').order_by('occur_name')
     return timelines.values_list('image_id', flat=True)
--- ImageManagement/timeline/timeline_spread.py
+++ ImageManagement/timeline/timeline_spread.py
@@ -1,13 +1,15 @@
-from .timeline_generator import *
 from users.models import MyUser
 from .models import Timeline
 
+# user上传了图片image
 def create_post_timeline(user, image):
     new_timeline = Timeline.objects.create(type='post', sender_id=user, image_id=image)
     new_timeline.original = new_timeline
     new_timeline.save()
 
+# user给timeline点了赞
 def create_like_timeline(user, timeline):
+    # 谁被点了赞
     receiver = timeline.original.sender_id if timeline.original.type == 'post'else timeline.original.receiver_id
     new_timeline = Timeline.objects.create(type='like', 
                                            original=timeline.original, 
@@ -16,6 +18,7 @@ def create_like_timeline(user, timeline):
                                            receiver_id=receiver)
     new_timeline.save()
 
+# user收藏了timeline上的图片
 def create_collect_timeline(user, timeline):
     receiver = timeline.original.sender_id if timeline.original.type == 'post'else timeline.original.receiver_id
     new_timeline = Timeline.objects.create(type='collect', 
@@ -25,6 +28,7 @@ def create_collect_timeline(user, timeline):
                                            receiver_id=receiver)
     new_timeline.save()
 
+# user给timeline上的图片评论了comment
 def create_comment_timeline(user, timeline, comment):
     receiver = timeline.original.sender_id if timeline.original.type == 'post'else timeline.original.receiver_id
     new_timeline = Timeline.objects.create(type='comment', 
