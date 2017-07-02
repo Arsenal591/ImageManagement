3d4dc0e9d61c50d742351ce29879d49146b042be
Arsenal591 <arsenal591@126.com>
2017-07-02 17:06:40 +0800

    add timeline generator

--- ImageManagement/timeline/models.py
+++ ImageManagement/timeline/models.py
@@ -3,11 +3,11 @@ from images.models import ImagePost
 from django.db import models
 from django.utils import timezone
 
-TIMELINE_TYPE = [('like', 'like'), ('collect', 'collect'), ('comment', 'comment')]
+TIMELINE_TYPE = [('like', 'like'), ('collect', 'collect'), ('comment', 'comment'), ('post', 'post')]
 class Timeline(models.Model):
     type = models.CharField(max_length=10, choices=TIMELINE_TYPE)
     sender_id = models.ForeignKey(MyUser, related_name='sends')
     image_id = models.ForeignKey(ImagePost)
-    receiver_id = models.ForeignKey(MyUser, related_name='receives')
+    receiver_id = models.ForeignKey(MyUser, related_name='receives', null=True)
     occur_time = models.TimeField(default=timezone.now)
     comment_text = models.CharField(default='', max_length=140, blank=True)
--- ImageManagement/timeline/timeline_generator.py
+++ ImageManagement/timeline/timeline_generator.py
@@ -1,5 +1,44 @@
-﻿from .models import Like, Collect, Comment
+﻿from .models import Timeline
 from users.models import MyUser
 
-def select_subject_timeline(username, start, end):
-    pass
+def select_object_timeline_by_user(request_user, target_user):
+    flag1 = (target_user in request_user.blacklist.all()) 
+    flag2 = (target_user in request_user.blacklist.all())
+
+    if flag1 > 0 or flag2 > 0:
+        print("blacked")
+        return Timeline.objects.none()
+    else:
+        result = Timeline.objects.filter(sender_id=request_user, image_id__is_public=1)
+        return result
+
+def select_object_timeline_by_name(request_name, target_name):
+    request_user = MyUser.objects.get(username=request_name)
+    target_user = MyUser.objects.get(username=target_name)
+
+    assert (request_user is not None) and (target_user is not None)
+
+    return select_object_timeline_by_user(request_user, target_user)
+
+
+def select_subject_timeline_by_user(user):
+    followings = user.followings.all()
+    timelines = Timeline.objects.none()
+
+    for follow in followings:
+        user_timeline = select_object_timeline_by_user(user, follow)
+        timelines = timelines | user_timeline
+
+    return timelines
+
+def select_subject_timeline_by_name(request_name):
+    request_user = MyUser.objects.get(username=request_name)
+
+    assert request_user is not None
+
+    return select_subject_timeline_by_user(request_user)
+
+def timeline_generate(type, sender, image, receiver, comment):
+    new_timeline = Timeline.objects.create(type=type, sender_id=sender, 
+                                           image_id=image, receiver_id=receiver, comment_text=comment)
+    new_timeline.save()
