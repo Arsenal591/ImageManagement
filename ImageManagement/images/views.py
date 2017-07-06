c4dff04f6c99a8280d525a5a1dfa27162caeef6a
VicaYang <vicayang496@gmail.com>
2017-07-06 12:16:56 +0800

    add del

--- ImageManagement/images/views.py
+++ ImageManagement/images/views.py
@@ -231,7 +231,7 @@ def pic(request, pic_id):
     pic = get_object_or_404(ImagePost, pk=pic_id)
     if_liked = Timeline.objects.filter(sender_id__username=request.user.username, type='like', image_id=pic).count()>0
     if_collected = Timeline.objects.filter(sender_id__username=request.user.username, type='collect', image_id=pic).count()>0
-    can_del = (pic.author.username == request.user.username)
+    can_del = (pic.author.id == request.user.id)
     tags = pic.tags.all()
     comments = Timeline.objects.filter(type='comment', image_id=pic)
     return render(request, 'pic.html', {'img': pic, 'if_liked': if_liked, 'if_collected': if_collected, 'tags':tags, 'can_del': can_del, 'comments':comments})
